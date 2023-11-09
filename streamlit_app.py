991d3579acd56e9a77a60958e98eaf7b87d7d0ae
SvenjaPf <150329097+SvenjaPf@users.noreply.github.com>
2023-11-09 16:19:04 +0100

    Update streamlit_app.py
    Correction of spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,5 +30,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 # display the data as a dataframe
-ruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
